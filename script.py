46683eece00eda875fff5edb29a0043be7ab3da9
GradientFox <53513063+GradientFox@users.noreply.github.com>
2020-11-14 10:12:19 +0500

    started pack

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for, session, make_response, flash
+import datetime
+from flask_sqlalchemy import SQLAlchemy
 
+
+app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'HelloworldByeWorld'
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
